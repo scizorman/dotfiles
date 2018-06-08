5b923dc3697817d5c6ac5f1e279856cb304d450d
Scizor-master <tueda1207@gmail.com>
2018-06-08 14:55:34 +0900

    Renamed.

---
+++ etc/init/osx/10_brew.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+cat << START
+
+************************************************
+START TO INSTALL HOMEBREW AND FORMULAS!!
+************************************************
+
+START
+
+
+#Install xcode
+printf "Installing \033[34mxcode-select\033[m...\n"
+if xcode-select --install > /dev/null 2>&1; then
+    xcode-select --install
+else
+    echo "  Already 'xcode-select' installed."
+fi
+
+
+# Install Homebrew
+printf "\nInstalling \033[34mHomebrew\033[m...\n"
+if which brew > /dev/null 2>&1; then
+    echo "  Already Homebrew installed."
+else
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+echo "\nRun 'brew doctor'"
+brew doctor
+
+echo "\nRun 'brew update'"
+brew update
+
+echo "\nRun 'brew upgrade'"
+brew upgrade
+
+# Install formulas
+formulas=(
+    git
+    tmux
+    "vim --with-lua"
+    neovim
+    python3
+)
+
+cask_formulas=(
+    docker
+)
+
+echo "\nInstalling formulas..."
+for cask_formula in "${cask_formulas}"; do
+    brew cask install $cask_formula || brew upgrade $cask_formula
+done
+
+for formula in "${formulas[@]}"; do
+  brew install $formula || brew upgrade $formula
+done
+
+echo ''
+echo "Run 'brew cleanup'"
+brew cleanup
+
+
+cat << END
+
+************************************************
+COMPLETE TO INSTALL HOMEBREW AND FORMULAS!!
+************************************************
+
+END
