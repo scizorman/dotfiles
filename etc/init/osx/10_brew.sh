1b2efe1ef7360ad710670828f87fe9da44b1bf2b
Scizor-master <tueda1207@gmail.com>
2018-06-08 22:01:18 +0900

    Modified

--- etc/init/osx/10_brew.sh
+++ etc/init/osx/10_brew.sh
@@ -11,32 +11,30 @@ if ! is_osx; then
     exit 1
 fi
 
-if has "brew"; then
-    log_pass "brew: Already installed."
-    exit
-fi
+# Install Homebrew
+if ! has "brew"; then
+    # Homebrew is dependent on 'xcode-select'.
+    if xcode-select --install >/dev/null 2>&1; then
+        log_info "Install 'xcode-select'."
+        xcode-select --install
+        log_pass "xcode-select: Installed successfully."
+    else
+        log_pass "xcode-select: Already installed."
+    fi
 
-# Homebrew is dependent on 'xcode-select'.
-if xcode-select --install >/dev/null 2>&1; then
-    log_info "Install 'xcode-select'."
-    xcode-select --install
-    log_pass "xcode-select: Installed successfully."
-else
-    log_pass "xcode-select: Already installed."
-fi
+    if ! has "ruby"; then
+        log_fail "Error: 'ruby' is required."
+        exit 1
+    fi
 
-if ! has "ruby"; then
-    log_fail "Error: 'ruby' is required."
-    exit 1
-fi
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-if has "brew"; then
-    brew doctor
-else
-    log_fail "Error: Failed to install Homebrew."
-    exit 1
+    if has "brew"; then
+        brew doctor
+    else
+        log_fail "Error: Failed to install Homebrew."
+        exit 1
+    fi
 fi
 
 log_pass "brew: Installed successfully."
