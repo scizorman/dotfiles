597fad11d3e863cc62744ca8f07a6f4469467e3c
Scizor-master <tueda1207@gmail.com>
2018-06-11 01:47:04 +0900

    Install tmux using yum or apt (Linux).

--- etc/init/common/tmux.sh
+++ etc/init/common/tmux.sh
@@ -25,8 +25,8 @@ if ! has "tmux"; then
             case "$(get_distribution)" in
                 redhat)
                     if has "yum"; then
-                        log_echo "Install packages required for Tmux with Yellowdog Updater Modified (YUM)."
-                        sudo yum -y install automake gcc libevent-devel ncurses-devel xsel
+                        log_echo "Install Tmux with Yellowdog Updater Modified (YUM)."
+                        sudo yum -y install tmux xsel
                     else
                         log_fail "Error: YUM is required."
                         exit 1
@@ -35,8 +35,8 @@ if ! has "tmux"; then
 
                 ubuntu)
                     if has "apt"; then
-                        log_echo "Install packages required for Tmux with Advanced Packaging Tool (APT)."
-                        sudo apt -y install build-essential automake libevent-dev ncurses-dev xsel
+                        log_echo "Install Tmux with Advanced Packaging Tool (APT)."
+                        sudo apt -y install tmux xsel
                     else
                         log_fail "Error: APT is required."
                         exit 1
@@ -48,24 +48,6 @@ if ! has "tmux"; then
                     exit 1
                     ;;
             esac
-
-            if [ -e $HOME/src ]; then
-                mkdir -p $HOME/src
-            fi
-
-            if has "git"; then
-                log_echo "Install Tmux with Git."
-
-                if [ ! -e $HOME/src/tmux ]; then
-                    git clone https://github.com/tmux/tmux.git $HOME/src/tmux
-                fi
-
-                cd $HOME/src/tmux && sh autogen.sh && ./configure && make
-                cp $HOME/src/tmux/tmux /usr/local/bin
-            else
-                log_fail "Git is required."
-                exit 1
-            fi
             ;;
 
         *)
