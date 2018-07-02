68936ee748f1702b6a85288b478b71bd70bcc770
Scizor-master <tueda1207@gmail.com>
2018-07-03 07:05:09 +0900

    Remove "compinit".

--- .zsh/50_misc.zsh
+++ .zsh/50_misc.zsh
@@ -1,15 +1,6 @@
 #
 # Completion
 #
-# autoload -Uz compinit
-# _comp_files=(${ZDOTDIR:-$HOME}/.zcompdump(Nm-20))
-# if (( $#_comp_files )); then
-#     compinit -i -C
-# else
-#     compinit -i
-# fi
-# unset _comp_files
-
 # Styles
 # Completing grouping
 zstyle ':completion:*:options' description 'yes'
