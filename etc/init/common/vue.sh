79d1c54556f6c8945aa394f50fe501b99d65485f
scizorman <tueda1207@gmail.com>
2018-12-30 01:44:26 +0900

    [add] Installer of Vue.js

---
+++ etc/init/common/vue.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Stop script if errors occure
+trap 'echo Error: $0:$LINENO stopped; exit 1' ERR INT
+set -eu
+
+# Get utilities
+. "$DOTFILES_PATH/etc/lib/vital.sh"
+
+# Install Vue.js
+if has 'vue'; then
+  log_pass 'Vue.js: Already installed!'
+else
+  if has 'npm'; then
+    log_echo 'Install Vue.js with npm'
+    if npm install -g vue-cli; then
+      log_pass 'Vue.js: Installed successfully!'
+    else
+      log_fail 'Vue.js: Failed to install'
+      exit 1
+    fi
+  else
+    log_fail 'Error: npm is required.'
+    exit 1
+  fi
+fi
