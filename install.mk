f68aaf49f4eeefde7621d03f19b5610a9d3324b6
scizorman <tueda1207@gmail.com>
2021-05-25 10:39:01 +0900

    [change] Remove Deno

--- install.mk
+++ install.mk
@@ -1,7 +1,7 @@
 .DEFAULT_GOAL := install
 
 .DELETE_ON_ERROR:
-.INTERMEDIATE: install-homebrew.sh get-poetry.py install-deno.sh google-cloud-sdk.tar.gz
+.INTERMEDIATE: install-homebrew.sh get-poetry.py google-cloud-sdk.tar.gz
 
 os := $(shell uname -s)
 ifeq ($(os),Linux)
@@ -34,16 +34,10 @@ rustup := $(HOME)/.cargo/bin/rustup
 $(rustup):
 	@curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-deno := $(HOME)/.deno/bin/deno
-install-deno.sh:
-	@curl -fsSL https://deno.land/x/install/install.sh -o $@
-$(deno): install-deno.sh
-	@sh $<
-
 google-cloud-sdk := $(HOME)/google-cloud-sdk
 $(google-cloud-sdk):
 	@curl https://sdk.cloud.google.com | bash
 
-install: $(brew) $(zinit) $(sdkman) $(poetry) $(rustup) $(deno) $(google-cloud-sdk)
+install: $(brew) $(zinit) $(sdkman) $(poetry) $(rustup) $(google-cloud-sdk)
 
 .PHONY: install
