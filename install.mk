6880ff91f48586d913b255e6a4a18567a4296e45
scizorman <tueda1207@gmail.com>
2021-09-21 21:13:03 +0900

    [change] Remove SDKMAN

--- install.mk
+++ install.mk
@@ -20,10 +20,6 @@ zinit := $(HOME)/.zsh/.zinit
 $(zinit):
 	@git clone https://github.com/zdharma/zinit.git $@/bin
 
-sdkman := $(HOME)/.sdkman
-$(sdkman):
-	@curl -s 'https://get.sdkman.io?rcupdate=false' | bash
-
 poetry := $(HOME)/.local/bin/poetry
 install-poetry.py:
 	@curl -sSL 'https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py' -o $@
