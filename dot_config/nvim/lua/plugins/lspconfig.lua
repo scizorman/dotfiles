d512a1b409887e8de316812c0348884e15d58167
scizorman <tueda1207@gmail.com>
2025-05-04 00:59:33 +0900

    fix: ensure formatting is applied before buffer write

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -38,6 +38,12 @@ local function setup()
         desc = "vim.lsp.buf.rename()",
       })
 
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        callback = function()
+          vim.lsp.buf.format({ async = true })
+        end,
+      })
+
       local client = vim.lsp.get_client_by_id(event.data.client_id)
       if client ~= nil and client:supports_method("textDocument/completion") then
         vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = true })
