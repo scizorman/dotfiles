e891bff1ba70eb63f13e441c70f2458229102821
scizorman <tueda1207@gmail.com>
2025-05-04 00:16:03 +0900

    fix: ensure client is not nil before checking completion support

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -43,7 +43,7 @@ local function setup()
       })
 
       local client = vim.lsp.get_client_by_id(event.data.client_id)
-      if client:supports_method("textDocument/completion") then
+      if client ~= nil and client:supports_method("textDocument/completion") then
         vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = true })
       end
     end,
