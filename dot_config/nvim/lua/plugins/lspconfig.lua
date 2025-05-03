6cbdc4f2d393fe221c17fdfa1dbf6abdf99be5f8
scizorman <tueda1207@gmail.com>
2025-05-04 00:08:23 +0900

    feat: add key mappings for LSP buffer actions

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -5,6 +5,16 @@ local function setup()
         virtual_text = { current_line = true },
       })
 
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = event.buf })
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = event.buf })
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = event.buf })
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = event.buf })
+      vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, { buffer = event.buf })
+      vim.keymap.set("n", "<Leader>a", vim.lsp.buf.code_action, { buffer = event.buf })
+      vim.keymap.set("n", "<Leader>k", vim.lsp.buf.hover, { buffer = event.buf })
+      vim.keymap.set("n", "<Leader>r", vim.lsp.buf.rename, { buffer = event.buf })
+      vim.keymap.set("n", "<Leader>d", vim.diagnostic.open_float, { buffer = event.buf })
+
       local client = vim.lsp.get_client_by_id(event.data.client_id)
       if client:supports_method("textDocument/completion") then
         vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = true })
