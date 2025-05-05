5cb91cb2f32ac8156c4ba1893ab3e7cf23b96e48
scizorman <tueda1207@gmail.com>
2025-05-05 20:09:10 +0900

    fix: ensure root directory and single file support are correctly configured in ts_ls.lua

--- dot_config/nvim/lsp/ts_ls.lua
+++ dot_config/nvim/lsp/ts_ls.lua
@@ -1,4 +1,6 @@
 --- @type vim.lsp.Config
 return {
+  root_dir = require("lspconfig.util").root_pattern("tsconfig.json", "jsconfig.json", "package.json"),
+  single_file_support = false,
   workspace_required = true,
 }
