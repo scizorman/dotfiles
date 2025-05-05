4291ca9377fa65c462a295c89a706c38f8660945
scizorman <tueda1207@gmail.com>
2025-05-05 20:10:16 +0900

    refactor: streamline nvim-treesitter plugin configuration

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,14 @@
+--- @type LazyPluginSpec
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufRead" },
-    build = ":TSUpdate",
-    main = "nvim-treesitter.configs",
-    opts = {
-      ensure_installed = "all",
-      sync_install = true,
-      auto_install = true,
-      highlight = { enable = true },
-      additional_vim_regex_highlighting = false,
-    },
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufRead" },
+  build = ":TSUpdate",
+  main = "nvim-treesitter.configs",
+  opts = {
+    ensure_installed = "all",
+    sync_install = true,
+    auto_install = true,
+    highlight = { enable = true },
+    additional_vim_regex_highlighting = false,
   },
 }
