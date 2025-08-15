cfd6d6e66f83af1a8523e22cf706e3617893febf
scizorman <tueda1207@gmail.com>
2025-08-15 23:55:44 +0900

    fix(nvim): change LSP format to synchronous with timeout

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -40,7 +40,7 @@ local function setup()
 
       vim.api.nvim_create_autocmd("BufWritePre", {
         callback = function()
-          vim.lsp.buf.format({ async = true })
+          vim.lsp.buf.format({ timeout_ms = 2000 })
         end,
       })
 
