7a6bf6f7656fcdbc3d9e894639dfa74ccce1cbd3
UEDA Tetsutaro <tueda1207@gmail.com>
2026-02-13 11:44:35 +0000

    feat(cloud): enable AWS CLI completion in zsh

--- modules/home/cloud.nix
+++ modules/home/cloud.nix
@@ -5,4 +5,11 @@
     awscli2
     google-cloud-sdk
   ];
+
+  programs.zsh = {
+    initContent = ''
+      autoload -Uz bashcompinit && bashcompinit
+      complete -C "$(which aws_completer)" aws
+    '';
+  };
 }
