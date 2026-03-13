cd7cfb5af91359a592c1bb5265b38fbb97f83e64
UEDA Tetsutaro <tueda1207@gmail.com>
2026-03-13 08:57:44 +0000

    feat(neovim): add canvas buffer toggle keymap

--- modules/home/neovim/config/lua/config/keymaps.lua
+++ modules/home/neovim/config/lua/config/keymaps.lua
@@ -31,3 +31,39 @@ vim.keymap.set("c", "<C-A>", "<Home>")
 vim.keymap.set("c", "<C-E>", "<End>")
 vim.keymap.set("c", "<C-D>", "<Del>")
 vim.keymap.set("c", "<C-Y>", "<C-R>*")
+
+local function find_canvas_bufnr()
+  for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.api.nvim_buf_is_valid(bufnr) and vim.b[bufnr].is_canvas then
+      return bufnr
+    end
+  end
+  return nil
+end
+
+local function create_canvas()
+  vim.cmd("enew")
+  local bufnr = vim.api.nvim_get_current_buf()
+  vim.b[bufnr].is_canvas = true
+  vim.bo[bufnr].buftype = "nofile"
+  vim.bo[bufnr].bufhidden = "hide"
+  vim.bo[bufnr].swapfile = false
+  vim.bo[bufnr].filetype = "markdown"
+  return bufnr
+end
+
+local function toggle_canvas()
+  local bufnr = find_canvas_bufnr()
+  if bufnr then
+    local winnr = vim.fn.bufwinnr(bufnr)
+    if winnr > 0 then
+      vim.cmd(winnr .. "wincmd w")
+    else
+      vim.cmd("buffer " .. bufnr)
+    end
+  else
+    create_canvas()
+  end
+end
+
+vim.keymap.set("n", "<Leader>c", toggle_canvas, { desc = "Toggle canvas buffer" })
